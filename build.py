2aa1f9b32b47f630f468ff71133bd5402ff32be8
Daniel Pokorny <danielp@kontent.ai>
2023-12-07 17:05:19 +0100

    fix build script

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ import PyInstaller.__main__
 
 PyInstaller.__main__.run([
     './src/logo.py',
-    '--distpath', '../dist',
+    '--distpath', './dist',
     '--onefile',
     '--windowed',
     '--add-data', './src/logoS.png;.'
